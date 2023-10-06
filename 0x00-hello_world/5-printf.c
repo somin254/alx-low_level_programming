4757a62236c3f0b24cf68d83ad23dfef19ce3a66
somin254 <sominrenson@gmail.com>
2023-10-06 05:33:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program which print using printf
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
