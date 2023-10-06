05dd99848ee750e2272c68684cf93c025c7bc40d
somin254 <sominrenson@gmail.com>
2023-10-06 07:06:45 -0700

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@ int main(void)
 {
 char q;
 int w;
-long long int e;
-long int r;
+long int e;
+long long int r;
 float t;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(q));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(r));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(t));
 return (0);
 }
