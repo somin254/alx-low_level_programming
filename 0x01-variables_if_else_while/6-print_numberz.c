d2a4d32bba5067274b84da42c852df1f38dda2ea
somin254 <sominrenson@gmail.com>
2023-10-09 13:24:54 -0700

    llk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** main - ogram that prints all single digit numbers of base
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
+
