41d2c04939fdb6ffead461efb0ef90aeb9b9892c
somin254 <sominrenson@gmail.com>
2023-10-09 12:41:06 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  program that prints all single digit numbers of base
+ * Returne: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
