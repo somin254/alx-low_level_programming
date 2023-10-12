0583d4026ffba852d67e3b4ed1b0ba2ec8f42cfe
somin254 <sominrenson@gmail.com>
2023-10-12 07:04:08 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 7-main.c 7-print_last_digit.c -o 7-last_digit
+julien@ubuntu:~/0x02$ ./7-last_digit 
+8044
+julien@ubuntu:~/0x02$ 
