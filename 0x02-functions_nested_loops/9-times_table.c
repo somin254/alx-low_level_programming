a26da3102917c4e4bbddc4d41bbe159bf6e013bc
somin254 <sominrenson@gmail.com>
2023-10-12 10:32:49 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,19 +10,19 @@ for (w = 0; w <= 9; w++)
 {
 for (r = 0; r <= 9; r++)
 {
-if (r == o)
+if (r == 0)
 {
-_putchar('0')
+_putchar('0');
 }
 else
 {
 _putchar(',');
 _putchar(' ');
-if ((w * j) < 10)
+if ((w * r) < 10)
 {
 _putchar(' ');
 }
-_putchar("%d", w * r);
+putchar("%d", w * r);
 }
 }
 _putchar('\n');
