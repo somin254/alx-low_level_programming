f4026bd8241d00df53a519ea10e8f27fb979bc01
somin254 <sominrenson@gmail.com>
2023-10-12 04:13:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print the alphabet, in lowercase
+ * Return: void
+ */
+void print_alphabet(void);
+{
+char r;
+for (r = 'a'; r <= 'z'; r++)
+_putchar(r);
+putchar('\n')
+}
