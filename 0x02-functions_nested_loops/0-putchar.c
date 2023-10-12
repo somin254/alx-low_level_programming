847b2beec4645a10139e243c0f3aa2f68a170f08
somin254 <sominrenson@gmail.com>
2023-10-12 03:28:41 -0700

    yes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-/* Add ons here */
-#endif /* MAIN_H */
-#include <stdio.h>
 #include <main.h>
+
 /**
- * main -a program that prints putchar
+ * main - put code
  * Return: 0
  */
 int main(void)
