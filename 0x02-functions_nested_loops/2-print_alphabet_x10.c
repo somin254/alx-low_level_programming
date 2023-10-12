906153deb6e993a0cc2b264fe434acf0161900c8
somin254 <sominrenson@gmail.com>
2023-10-12 04:49:12 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char r;
+int w;
+for (w = 0; w < 10; w++)
+{
+for (r = 'a'; r <= 'z'; r++)
+_putichar(r);
+_putchar('\n');
+}
+}
