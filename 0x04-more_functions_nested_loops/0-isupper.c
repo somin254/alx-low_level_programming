c7b258cfd49623c0983771156827c01e795ecb3a
somin254 <sominrenson@gmail.com>
2023-10-13 15:24:50 -0700

    jjj

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,37 @@
 #include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
+
+0-main.c
+#include "main.h"
 #include <stdio.h>
 
 /**
- * _isupper - check the code.
- *@c: the character to be cheacked
+ * main - check the code.
+ *
  * Return: Always 0.
  */
-int _isupper(int c)
+int main(void)
 {
-char c;
-c = 'A';
-printf("%c: %d\n", c, _isupper(0));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-return (0);
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
