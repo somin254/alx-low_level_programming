743ab1e64a45c1a3c0633054255b4b201dee48bb
somin254 <sominrenson@gmail.com>
2023-10-24 00:58:28 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 /**
 * set_string - Entry point
-* @*to: input
-* @**s: input
+* @to: input
+* @s: input
 * Return: Always 0 (Success)
 */
 void set_string(char **s, char *to)
 {
-    *s = to;
+*s = to;
 }
