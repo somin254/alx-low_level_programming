e1fb955488326dacf665e45e9e78a3f987dde014
somin254 <sominrenson@gmail.com>
2023-10-25 01:42:08 -0700

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,16 @@
 */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
-_putchar(*s);
+putchar('\n');
+return;
+}
+putchar(*s);
 _puts_recursion(s + 1);
 }
-else
-_putchar('\n');
+int main(void)
+{
+_puts_recursion("Puts with recursion");
+return (0);
 }
