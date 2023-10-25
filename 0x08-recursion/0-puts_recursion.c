865fbe0c45a7142a028d1e564076f12ecd62c8b9
somin254 <sominrenson@gmail.com>
2023-10-25 03:03:08 -0700

    3-factorial.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,27 +3,16 @@
 * _puts_recursion - main function
 *
 * @s: function parameter
-* _putchar - print the out put
-* main - the main function
+*
 * Return: Always 0.
-* @c: the parametr
 */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
+if (*s)
 {
-putchar('\n');
-return;
-}
 _putchar(*s);
 _puts_recursion(s + 1);
 }
-void _putchar(char c)
-{
-_putchar(c);
-}
-int main(void)
-{
-_puts_recursion("Puts with recursion");
-return (0);
+else
+_putchar('\n');
 }
