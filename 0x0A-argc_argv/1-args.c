5a512ea7e0ab5cfd54e1d70d44dc95fb48e19e98
somin254 <sominrenson@gmail.com>
2023-10-28 15:25:42 -0700

    ./nargs hello, world

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main- main function
+ *@argc: used to count
+ *@argv: used to count vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
