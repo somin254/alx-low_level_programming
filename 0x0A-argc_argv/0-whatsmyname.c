0515ee0d84e0b70fb04f275a78da0b2df96ea654
somin254 <sominrenson@gmail.com>
2023-10-28 14:55:52 -0700

    argc, argv$

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prongram name
+ * @argc: argument count
+ * @argv: arguments vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
