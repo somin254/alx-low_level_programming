6871952dfa9d1f21642a5b65d8ddb92dea92d1ad
somin254 <sominrenson@gmail.com>
2023-10-31 09:01:10 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+char *copy;
+int i, r = 0;
+if (str == NULL)
+return (NULL);
+i = 0;
+while (str[i] != '\0')
+	i++;
+copy = malloc(sizeof(char) * (i + 1));
+if (copy == NULL)
+	return (NULL);
+for (r = 0; str[r]; r++)
+	copy[r] = str[r];
+return (copy);
+}
