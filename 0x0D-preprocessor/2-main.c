10ff6016c35392350e273afe078be3f511f78471
somin254 <sominrenson@gmail.com>
2023-11-07 01:48:37 -0800

    object code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - print the name of the file neded
- * Return: 0
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("%s\n", _FILE_);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
