ec5a1fee51badfe313a65241a8135ddd6e3549d9
somin254 <sominrenson@gmail.com>
2023-11-21 07:01:48 -0800

    commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -29,6 +30,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-int _putchar(char c)
 
 #endif
