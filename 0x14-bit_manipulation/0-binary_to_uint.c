fa4724aa103e2b5a452f58442e2406264580ab06
somin254 <sominrenson@gmail.com>
2023-11-24 09:43:02 -0800

    binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *binary_to_uint - this is the binary unit
+ *@b: parameter used as apointer
+ *Return: 0
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b);
+char *b
+{
+unsigned int result = 0;
+if (b == NULL)
+return (0);
+while (*b != '\0')
+{
+if (*b != '0' && *b != '1')
+return (0);
+result <<= 1;
+result += (*b - '0');
+b++;
+}
+return (result);
+}
